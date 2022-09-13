ad7ab1c2be02b3c450218056f75e64d0bc820207
GFrancV <gfrancv@hotmail.com>
2022-09-13 11:00:18 +0100

    Init the project.

--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+//Bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+
 createApp(App).use(store).use(router).mount("#app");
