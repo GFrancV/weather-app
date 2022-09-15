0fe7c5ce8a146d4005a28b7bee341228c7031030
GFrancV <gfrancv@hotmail.com>
2022-09-15 16:34:25 +0100

    Updates

--- js/about.78f0e29a.js
+++ js/about.78f0e29a.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkweather_app"]=self["webpackChunkweather_app"]||[]).push([[443],{7381:function(a,e,n){n.r(e),n.d(e,{default:function(){return o}});var t=n(3396);const r={class:"about"},u=(0,t._)("h1",null,"This is an about page",-1),s=[u];function c(a,e){return(0,t.wg)(),(0,t.iD)("div",r,s)}var i=n(89);const p={},h=(0,i.Z)(p,[["render",c]]);var o=h}}]);
-//# sourceMappingURL=about.e8823bf0.js.map
+//# sourceMappingURL=about.78f0e29a.js.map
